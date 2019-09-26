cb0d00453f9bca3d98750d20e5f323c7b221488a
devannea <devannenotes@gmail.com>
2019-09-25 19:35:47 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Time } from '@angular/common';
+import { Timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+  todoTime: Time;
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false },
+    
+    ];
+  }
+  // adds a todo to our list
+  addTodo():void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
+  }  
+  // a method to delete an item
+  deleteTodo(todo:any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
